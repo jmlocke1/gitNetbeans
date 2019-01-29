7b7157656f59fa130aa5bb6810a8ffc4700494f1
jmlocke1 <josemidaw@gmail.com>
2019-01-29 23:19:50 +0100

    Primer cambio al proyecto

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Práctica Git</title>
+</head>
+<body>
+    <h1>Creando el proyecto con Git</h1>
+    <p>Añadimos un cambio al proyecto</p>
+</body>
+</html>
